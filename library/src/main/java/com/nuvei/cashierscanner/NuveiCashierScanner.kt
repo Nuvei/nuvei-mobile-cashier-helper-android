9ac06830aa765c8e7873cbb0ae4b86914d615d85
Michael Kessler <michael@mobaxe.com>
2021-07-13 12:25:27 +0300

    Added credit card scanning option implementation

--- library/src/main/java/com/nuvei/cashierscanner/NuveiCashierScanner.kt
+++ library/src/main/java/com/nuvei/cashierscanner/NuveiCashierScanner.kt
@@ -1,17 +1,26 @@
 package com.nuvei.cashierscanner
 
+import android.R.attr
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Intent
 import android.net.Uri
 import android.util.Base64
 import android.webkit.WebView
+import cards.pay.paycardsrecognizer.sdk.Card
+import cards.pay.paycardsrecognizer.sdk.ScanCardIntent
 import com.google.zxing.integration.android.IntentIntegrator
 import java.lang.ref.WeakReference
 
+
+@SuppressLint("StaticFieldLeak")
 public object NuveiCashierScanner {
 
     private const val messageName = "sccardscanner"
     const val SCAN_CARD_REQUEST_CODE = 8493
+
+    private var source = ""
+
 
     private val hostWhiteList = arrayListOf(
         "apmtest.gate2shop.com",// QA
@@ -29,10 +38,17 @@ public object NuveiCashierScanner {
 
     public fun handleURL(url: Uri?, activity: Activity) =
         url?.takeIf { it.toString().contains("nuveicashier://scanQR", ignoreCase = true) }?.let {
+            source = "scanQR"
             val integrator = IntentIntegrator(activity)
             integrator.setOrientationLocked(false)
             integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE)
             integrator.initiateScan()
+
+            true
+        } ?: url?.takeIf { it.toString().contains("nuveicashier://scanCard", ignoreCase = true) }?.let {
+            source = "scanCard"
+            val intent = ScanCardIntent.Builder(activity).build()
+            activity.startActivityForResult(intent, SCAN_CARD_REQUEST_CODE)
 
             true
         } ?: false
@@ -41,19 +57,44 @@ public object NuveiCashierScanner {
         IntentIntegrator.parseActivityResult(requestCode, resultCode, data)?.contents?.let { result ->
             didScan(result)
             true
+        } ?: parseActivityResultAsCreditCard(requestCode, resultCode, data)?.let { card ->
+            didScan(card)
+            true
         } ?: false
+
+    private fun parseActivityResultAsCreditCard(requestCode: Int, resultCode: Int, data: Intent?): Card? {
+        return if (requestCode != SCAN_CARD_REQUEST_CODE || resultCode != Activity.RESULT_OK) {
+            null
+        } else {
+            data?.getParcelableExtra(ScanCardIntent.RESULT_PAYCARDS_CARD)
+        }
+    }
 
     private fun didScan(qrString: String) {
         updateCashier("{\"qrCode\":\"${qrString}\"}")
     }
 
+    private fun didScan(card: Card) {
+        updateCashier(
+            "{" +
+                    "\"source\":\"$source\"" +
+                    ",\"status\":\"OK\"" +
+                    ",\"cardHolderName\":\"${card.cardHolderName}\"" +
+                    ",\"cardNumber\":\"${card.cardNumber}\"" +
+                    ",\"expDate\":\"${card.expirationDate}\"" +
+                    "}"
+        )
+    }
+
     private fun didFail(error: SCCardScannerError) {
-        updateCashier("{" +
-                "\"source\":\"scanCard\"" +
-                ",\"status\":\"NOK\"" +
-                ",\"errorCode\":\"${error.code()}\"" +
-                ",\"errorMessage\":\"${error.description()}\"" +
-                "}")
+        updateCashier(
+            "{" +
+                    "\"source\":\"$source\"" +
+                    ",\"status\":\"NOK\"" +
+                    ",\"errorCode\":\"${error.code()}\"" +
+                    ",\"errorMessage\":\"${error.description()}\"" +
+                    "}"
+        )
 //        val js = """
 //            window.postMessage({
 //            source: 'scanCard',
